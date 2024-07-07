2f57d4e132edbcb70a8c2eb805dbf99a22fde469
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-07-07 14:37:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
